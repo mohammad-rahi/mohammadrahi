58b5105eecf89173c8c8a2f91353b4009a1dd6af
Mohammad Rahi <85076632+mohammad-rahi@users.noreply.github.com>
2022-09-28 22:44:16 +0600

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -51,19 +51,19 @@ const Footer = () => {
                 </div>
                 <div className="grid lg:hidden grid-cols-2 text-sm py-4 px-3">
                     <div>
-                        <p className="cursor-pointer my-2">Amazon.com</p>
-                        <p className="cursor-pointer my-2">Your Lists</p>
-                        <p className="cursor-pointer my-2">Find a Gift</p>
-                        <p className="cursor-pointer my-2">Browsing History</p>
-                        <p className="cursor-pointer my-2">Returns</p>
+                        <p className="cursor-pointer my-3">Amazon.com</p>
+                        <p className="cursor-pointer my-3">Your Lists</p>
+                        <p className="cursor-pointer my-3">Find a Gift</p>
+                        <p className="cursor-pointer my-3">Browsing History</p>
+                        <p className="cursor-pointer my-3">Returns</p>
                     </div>
                     <div>
-                        <p className="cursor-pointer my-2">Your Orders</p>
-                        <p className="cursor-pointer my-2">Gift Cards &amp; Registry</p>
-                        <p className="cursor-pointer my-2">Your Account</p>
-                        <p className="cursor-pointer my-2">Sell products on Amazon</p>
-                        <p className="cursor-pointer my-2">Your Recalls and Product Safety Alerts</p>
-                        <p className="cursor-pointer my-2">Customar Service</p>
+                        <p className="cursor-pointer my-3">Your Orders</p>
+                        <p className="cursor-pointer my-3">Gift Cards &amp; Registry</p>
+                        <p className="cursor-pointer my-3">Your Account</p>
+                        <p className="cursor-pointer my-3">Sell products on Amazon</p>
+                        <p className="cursor-pointer my-3">Your Recalls and Product Safety Alerts</p>
+                        <p className="cursor-pointer my-3">Customar Service</p>
                     </div>
                 </div>
                 <span className="h-0 lg:h-[1px] bg-slate-600 block" ></span>
@@ -246,7 +246,7 @@ const Footer = () => {
                         </div>
                     </div>
                 </div>
-                <div className="grid grid-cols-3 lg:hidden gap-4 text-sm">
+                <div className="grid grid-cols-3 lg:hidden gap-4 text-sm mb-2">
                     <div className="flex items-center justify-center p-1 gap-1 cursor-pointer">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
