95bfce44c1e059c1621293b79cfce62fa502f193
mohammad-rahi <mohammadrahi00318@gmail.com>
2022-09-30 16:35:45 +0600

    update firebase

--- firebase/index.js
+++ firebase/index.js
@@ -3,14 +3,24 @@ import { getAuth } from "firebase/auth";
 // import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyDjiEd9D4evGO69X6br1KesniVKTfwH2mk",
+    authDomain: "fir-c1353.firebaseapp.com",
+    projectId: "fir-c1353",
+    storageBucket: "fir-c1353.appspot.com",
+    messagingSenderId: "909286649781",
+    appId: "1:909286649781:web:f59bba57db48ef7c6a73fc",
+    measurementId: "G-ZW2V1T4CHG"
 };
+
+// const firebaseConfig = {
+//     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+//     measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+// };
 
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
